bacc222e46d5660dd1fe1b5079f9d9f0b2dbd829
Dolu <alfrider02@gmail.com>
2022-07-24 12:56:46 +0200

    feat(lua): nui callback for time & weather

--- client/world/world.lua
+++ client/world/world.lua
@@ -1,11 +1,35 @@
+-- Global variables
+hour, minute, second, weather, freezeTime = 12, 30, 0, 'CLEAR', false
+
+-- NUI callbacks
 RegisterNUICallback('dmt:world:setTime', function(data, cb)
-    local hours = 12
-    FUNC.setClock(data.hour, data.minute, 0)
-    cb({hours})
+	if not data then print("^1[WARNING]^7 Time slider seems to be broken!") return end
+
+	FUNC.setClock(hour, minute, second)
+
+	-- Updating global variables
+	hour = data.hour or hour
+	minute = data.minute or minute
+	second = data.second or second
+
+	cb({})
 end)
 
 RegisterNUICallback('dmt:world:setWeather', function(data, cb)
-    local weather = 'clear'
-    FUNC.setWeather(data.weather)
-    cb({hours})
+	if not data then print("^1[WARNING]^7 Weather slider seems to be broken!") return end
+
+	FUNC.setWeather(weather)
+
+	-- Updating global variables
+	weather = data and data.weather or weather
+
+	cb({})
+end)
+
+RegisterNUICallback('dmt:world:freezeTime', function(data, cb)
+	if not data then print("^1[WARNING]^7 Freeze Time checkbox seems to be broken!") return end
+
+	-- Interacting with loop (../threads.lua) + updating related global variable
+	freezeTime = data.freezeTime or freezeTime
+	cb({false})
 end)
