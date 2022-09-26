58d5fbed010799503493b170a3b4daeed5cb2657
Dolu <alfrider02@gmail.com>
2022-09-26 04:25:00 +0200

    feat(web/client): sending interior data

---
+++ client/interior/main.lua
@@ -0,0 +1,69 @@
+-- Check for interior data
+local lastRoomId = 0
+local function GetInteriorData(fromThread)
+    local playerPed = PlayerPedId()
+    local interiorId = GetInteriorFromEntity(playerPed)
+    local currentRoomHash = GetRoomKeyFromEntity(playerPed)
+    local currentRoomId = GetInteriorRoomIndexByHash(interiorId, currentRoomHash)
+
+    if (fromThread and lastRoomId ~= currentRoomId) or not fromThread then
+        lastRoomId = currentRoomId
+        local roomCount = GetInteriorRoomCount(interiorId) - 1
+        local portalCount = GetInteriorPortalCount(interiorId)
+
+        local rooms = {}
+        for i = 1, roomCount do
+            rooms[i] = {
+                roomId = i,
+                name = GetInteriorRoomName(interiorId, i),
+                flag = GetInteriorRoomFlag(interiorId, i),
+                timecycle = tostring(GetInteriorRoomTimecycle(interiorId, i)),
+                isCurrent = currentRoomId == i and true or nil
+            }
+        end
+
+        local portals = {}
+        for i = 0, portalCount - 1 do
+            portals[i] = {
+                flag = GetInteriorPortalFlag(interiorId, i),
+                roomFrom = GetInteriorPortalRoomFrom(interiorId, i),
+                roomTo = GetInteriorPortalRoomTo(interiorId, i)
+            }
+        end
+
+        local intData = {
+            interiorId = interiorId,
+            roomCount = roomCount,
+            portalCount = portalCount,
+            rooms = rooms,
+            portals = portals,
+            currentRoom = {
+                currentRoomId = currentRoomId > 0 and currentRoomId or 0,
+                currentRoomName = currentRoomId > 0 and rooms[currentRoomId].name or 'none',
+                currentRoomFlag = currentRoomId > 0 and rooms[currentRoomId].flag or 0,
+                currentRoomTimecycle = currentRoomId > 0 and rooms[currentRoomId].timecycle or 0
+            },
+            currentRoomName = currentRoomId > 0 and rooms[currentRoomId].name or 'none'
+        }
+
+        SendNUIMessage({
+            action = 'setIntData',
+            data = intData
+        })
+    else
+        if interiorId == 0 then
+            SendNUIMessage({
+                action = 'setIntData',
+                data = { interiorId = 0 }
+            })
+        end
+        Wait(500)
+    end
+end
+
+CreateThread(function()
+    while true do
+        GetInteriorData(true)
+        Wait(0)
+    end
+end)
