f922ea797fdbdc59a552c94f250ae45eeed0e6b8
Dolu <alfrider02@gmail.com>
2022-09-28 15:43:06 +0200

    fix(noclip): handle isMenuOpen & right click to rotate

--- client/freecam/main.lua
+++ client/freecam/main.lua
@@ -8,182 +8,194 @@ local CONTROLS = _G.CONTROL_MAPPING
 
 -------------------------------------------------------------------------------
 local function GetSpeedMultiplier()
-  local fastNormal = GetSmartControlNormal(CONTROLS.MOVE_FAST)
-  local slowNormal = GetSmartControlNormal(CONTROLS.MOVE_SLOW)
+	local fastNormal = GetSmartControlNormal(CONTROLS.MOVE_FAST)
+	local slowNormal = GetSmartControlNormal(CONTROLS.MOVE_SLOW)
 
-  local baseSpeed = SETTINGS.BASE_MOVE_MULTIPLIER
-  local fastSpeed = 1 + ((SETTINGS.FAST_MOVE_MULTIPLIER - 1) * fastNormal)
-  local slowSpeed = 1 + ((SETTINGS.SLOW_MOVE_MULTIPLIER - 1) * slowNormal)
+	local baseSpeed = SETTINGS.BASE_MOVE_MULTIPLIER
+	local fastSpeed = 1 + ((SETTINGS.FAST_MOVE_MULTIPLIER - 1) * fastNormal)
+	local slowSpeed = 1 + ((SETTINGS.SLOW_MOVE_MULTIPLIER - 1) * slowNormal)
 
-  local frameMultiplier = GetFrameTime() * 60
-  local speedMultiplier = baseSpeed * fastSpeed / slowSpeed
+	local frameMultiplier = GetFrameTime() * 60
+	local speedMultiplier = baseSpeed * fastSpeed / slowSpeed
 
-  return speedMultiplier * frameMultiplier
+	return speedMultiplier * frameMultiplier
 end
 
 local function UpdateCamera()
-  if not IsFreecamActive() or IsPauseMenuActive() then
-    return
-  end
+	if not IsFreecamActive() or IsPauseMenuActive() then
+		return
+	end
 
-  if not IsFreecamFrozen() then
-    local vecX, vecY = GetFreecamMatrix()
-    local vecZ = vector3(0, 0, 1)
+	if not IsFreecamFrozen() then
+		local vecX, vecY = GetFreecamMatrix()
+		local vecZ = vector3(0, 0, 1)
 
-    local pos = GetFreecamPosition()
-    local rot = GetFreecamRotation()
+		local pos = GetFreecamPosition()
+		local rot = GetFreecamRotation()
 
-    -- Get speed multiplier for movement
-    local speedMultiplier = GetSpeedMultiplier()
+		-- Get speed multiplier for movement
+		local speedMultiplier = GetSpeedMultiplier()
 
-    -- Get rotation input
-    local lookX = GetSmartControlNormal(CONTROLS.LOOK_X)
-    local lookY = GetSmartControlNormal(CONTROLS.LOOK_Y)
+		-- Get rotation input
+		local lookX
+		local lookY
+		if isMenuOpen then
+			if IsDisabledControlPressed(0, 25) then
+				lookX = GetSmartControlNormal(CONTROLS.LOOK_X)
+				lookY = GetSmartControlNormal(CONTROLS.LOOK_Y)
+            else
+                lookX = 0
+                lookY = 0
+            end
+		else
+            lookX = GetSmartControlNormal(CONTROLS.LOOK_X)
+            lookY = GetSmartControlNormal(CONTROLS.LOOK_Y)
+        end
 
-    -- Get position input
-    local moveX = GetSmartControlNormal(CONTROLS.MOVE_X)
-    local moveY = GetSmartControlNormal(CONTROLS.MOVE_Y)
-    local moveZ = GetSmartControlNormal(CONTROLS.MOVE_Z)
+		-- Get position input
+		local moveX = GetSmartControlNormal(CONTROLS.MOVE_X)
+		local moveY = GetSmartControlNormal(CONTROLS.MOVE_Y)
+		local moveZ = GetSmartControlNormal(CONTROLS.MOVE_Z)
 
-    -- Calculate new rotation.
-    local rotX = rot.x + (-lookY * SETTINGS.LOOK_SENSITIVITY_X)
-    local rotZ = rot.z + (-lookX * SETTINGS.LOOK_SENSITIVITY_Y)
-    local rotY = rot.y
+		-- Calculate new rotation.
+		local rotX = rot.x + (-lookY * SETTINGS.LOOK_SENSITIVITY_X)
+		local rotZ = rot.z + (-lookX * SETTINGS.LOOK_SENSITIVITY_Y)
+		local rotY = rot.y
 
-    -- Adjust position relative to camera rotation.
-    pos = pos + (vecX * moveX * speedMultiplier)
-    pos = pos + (vecY * -moveY * speedMultiplier)
-    pos = pos + (vecZ * moveZ * speedMultiplier)
+		-- Adjust position relative to camera rotation.
+		pos = pos + (vecX * moveX * speedMultiplier)
+		pos = pos + (vecY * -moveY * speedMultiplier)
+		pos = pos + (vecZ * moveZ * speedMultiplier)
 
-    -- Adjust new rotation
-    rot = vector3(rotX, rotY, rotZ)
+		-- Adjust new rotation
+		rot = vector3(rotX, rotY, rotZ)
 
-    -- Update camera
+		-- Update camera
 
-    if pos ~= GetFreecamPosition() then
-      SetFreecamPosition(pos)
-    end
+		if pos ~= GetFreecamPosition() then
+			SetFreecamPosition(pos)
+		end
 
-    SetFreecamRotation(rot)
+		SetFreecamRotation(rot)
 
-    return pos, rotZ
-  end
+		return pos, rotZ
+	end
 
-  -- Trigger a tick event. Resources depending on the freecam position can
-  -- make use of this event.
-  -- TriggerEvent('freecam:onTick')
+	-- Trigger a tick event. Resources depending on the freecam position can
+	-- make use of this event.
+	-- TriggerEvent('freecam:onTick')
 end
 
 -------------------------------------------------------------------------------
 function StartFreecamThread()
-  -- Camera/Pos updating thread
-  Citizen.CreateThread(function()
-    local ped = cache.ped
-    SetFreecamPosition(GetEntityCoords(ped))
+	-- Camera/Pos updating thread
+	Citizen.CreateThread(function()
+		local ped = cache.ped
+		SetFreecamPosition(GetEntityCoords(ped))
 
-    local function updatePos(pos, rotZ)
-      -- Update ped
-      SetEntityCoords(ped, pos.x, pos.y, pos.z)
+		local function updatePos(pos, rotZ)
+			-- Update ped
+			SetEntityCoords(ped, pos.x, pos.y, pos.z)
 
-      -- Update veh
-      local veh = cache.seat == -1 and cache.vehicle
+			-- Update veh
+			local veh = cache.seat == -1 and cache.vehicle
 
-      if veh then
-        SetEntityCoords(veh, pos.x, pos.y, pos.z)
-      end
+			if veh then
+				SetEntityCoords(veh, pos.x, pos.y, pos.z)
+			end
 
-      SetEntityHeading(ped, rotZ)
-    end
+			SetEntityHeading(ped, rotZ)
+		end
 
-    local frameCounter = 0
-    local loopPos, loopRotZ
-    while IsFreecamActive() do
-      loopPos, loopRotZ = UpdateCamera()
-      frameCounter += 1
-      if frameCounter > 100 then
-        frameCounter = 0
-        updatePos(loopPos, loopRotZ)
-      end
-      Wait(0)
-    end
+		local frameCounter = 0
+		local loopPos, loopRotZ
+		while IsFreecamActive() do
+			loopPos, loopRotZ = UpdateCamera()
+			frameCounter += 1
+			if frameCounter > 100 then
+				frameCounter = 0
+				updatePos(loopPos, loopRotZ)
+			end
+			Wait(0)
+		end
 
-    -- One last time due to the optimization
-    updatePos(loopPos, loopRotZ)
-  end)
+		-- One last time due to the optimization
+		updatePos(loopPos, loopRotZ)
+	end)
 
-  local function InstructionalButton(controlButton, text)
-    ScaleformMovieMethodAddParamPlayerNameString(controlButton)
-    BeginTextCommandScaleformString("STRING")
-    AddTextComponentScaleform(text)
-    EndTextCommandScaleformString()
-  end
+	local function InstructionalButton(controlButton, text)
+		ScaleformMovieMethodAddParamPlayerNameString(controlButton)
+		BeginTextCommandScaleformString("STRING")
+		AddTextComponentScaleform(text)
+		EndTextCommandScaleformString()
+	end
 
-  --Scaleform drawing thread
-  Citizen.CreateThread(function()
-    local scaleform = RequestScaleformMovie("instructional_buttons")
-    while not HasScaleformMovieLoaded(scaleform) do
-      Wait(1)
-    end
-    PushScaleformMovieFunction(scaleform, "CLEAR_ALL")
-    PopScaleformMovieFunctionVoid()
+	--Scaleform drawing thread
+	Citizen.CreateThread(function()
+		local scaleform = RequestScaleformMovie("instructional_buttons")
+		while not HasScaleformMovieLoaded(scaleform) do
+			Wait(1)
+		end
+		PushScaleformMovieFunction(scaleform, "CLEAR_ALL")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_CLEAR_SPACE")
-    PushScaleformMovieFunctionParameterInt(200)
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_CLEAR_SPACE")
+		PushScaleformMovieFunctionParameterInt(200)
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(0)
-    InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_FAST, 1), "Faster")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+		PushScaleformMovieFunctionParameterInt(0)
+		InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_FAST, 1), "Faster")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(1)
-    InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_SLOW, 1), "Slower")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+		PushScaleformMovieFunctionParameterInt(1)
+		InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_SLOW, 1), "Slower")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(2)
-    InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_Y, 1), "Fwd/Back")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+		PushScaleformMovieFunctionParameterInt(2)
+		InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_Y, 1), "Fwd/Back")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(3)
-    InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_X, 1), "Left/Right")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+		PushScaleformMovieFunctionParameterInt(3)
+		InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_X, 1), "Left/Right")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(4)
-    InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_Z[2], 1), "Down")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+		PushScaleformMovieFunctionParameterInt(4)
+		InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_Z[2], 1), "Down")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(5)
-    InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_Z[1], 1), "Up")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+		PushScaleformMovieFunctionParameterInt(5)
+		InstructionalButton(GetControlInstructionalButton(0, CONTROLS.MOVE_Z[1], 1), "Up")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "DRAW_INSTRUCTIONAL_BUTTONS")
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "DRAW_INSTRUCTIONAL_BUTTONS")
+		PopScaleformMovieFunctionVoid()
 
-    PushScaleformMovieFunction(scaleform, "SET_BACKGROUND_COLOUR")
-    PushScaleformMovieFunctionParameterInt(0)
-    PushScaleformMovieFunctionParameterInt(0)
-    PushScaleformMovieFunctionParameterInt(0)
-    PushScaleformMovieFunctionParameterInt(80)
-    PopScaleformMovieFunctionVoid()
+		PushScaleformMovieFunction(scaleform, "SET_BACKGROUND_COLOUR")
+		PushScaleformMovieFunctionParameterInt(0)
+		PushScaleformMovieFunctionParameterInt(0)
+		PushScaleformMovieFunctionParameterInt(0)
+		PushScaleformMovieFunctionParameterInt(80)
+		PopScaleformMovieFunctionVoid()
 
-    while IsFreecamActive() do
-      DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255, 0)
-      Wait(0)
-    end
-    SetScaleformMovieAsNoLongerNeeded()
-  end)
+		while IsFreecamActive() do
+			DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255, 0)
+			Wait(0)
+		end
+		SetScaleformMovieAsNoLongerNeeded()
+	end)
 end
 
 --------------------------------------------------------------------------------
 
 -- When the resource is stopped, make sure to return the camera to the player.
 AddEventHandler('onResourceStop', function(resourceName)
-  if resourceName == GetCurrentResourceName() then
-    SetFreecamActive(false)
-  end
+	if resourceName == GetCurrentResourceName() then
+		SetFreecamActive(false)
+	end
 end)
