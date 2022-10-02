d41dd22268a3547034dcaae2832192629dd5a062
Dolu <alfrider02@gmail.com>
2022-10-02 13:59:16 +0200

    change(web): stylized home tab

--- web/src/layouts/menu/views/home/index.tsx
+++ web/src/layouts/menu/views/home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, Stack, SimpleGrid, Button, Paper, Group } from '@mantine/core'
+import { Text, Stack, SimpleGrid, Button, Paper, Group, Space } from '@mantine/core'
 import { openModal } from '@mantine/modals'
 import { useState } from 'react'
 import { GiTeleport } from 'react-icons/gi'
@@ -29,11 +29,13 @@ const Home: React.FC = () => {
             <Text size={20} weight={600}>Current Coords</Text>
             <ImLocation size={24}/>
           </Group>
+          
+          <Space h="sm" />
 
-          <Text>Coords: {currentCoords}</Text>
+          <Group><Text>Coords:</Text><Text color="blue.4" >{currentCoords}</Text></Group>
           
           <Group position='apart'>
-            <Text>Heading: {currentHeading}</Text>
+            <Group><Text>Heading:</Text><Text color="blue.4" >{currentHeading}</Text></Group>
             <Button
               color='blue.4'
               variant='outline'
@@ -57,11 +59,13 @@ const Home: React.FC = () => {
             <Text size={20} weight={600}>Last location</Text>
             <GiTeleport size={24} />
           </Group>
+          
+          <Space h="sm" />
 
-          <Text>Name: {location.name}</Text>
+          <Group><Text>Name:</Text><Text color="blue.4" >{location.name}</Text></Group>
           
           <Group position='apart'>
-            <Text>Coords: {location.x}, {location.y}, {location.z}</Text>
+            <Group><Text>Coords:</Text><Text color="blue.4" >{location.x}, {location.y}, {location.z}</Text></Group>
             <Button
               color='blue.4'
               variant='outline'
@@ -89,7 +93,10 @@ const Home: React.FC = () => {
                 <Group><Text>Current room:</Text><Text color="blue.4" >{interior.currentRoom?.index} - {interior.currentRoom?.name}</Text></Group>
               </>
             : 
-              "Not inside any interior"
+              <>
+                <Space h="sm" />
+                <Text color="red.4">You are not inside any interior.</Text>
+              </>
           }
         </Paper>
       </Stack>
