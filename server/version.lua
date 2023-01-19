898eb44f0f549b99468ebdd4aa09a1b451cb5edf
Dolu <alfrider02@gmail.com>
2023-01-19 12:00:15 +0100

    fix(server): better resource version check

--- server/version.lua
+++ server/version.lua
@@ -1,24 +1,23 @@
 local versionData
 
 local function checkVersion()
-    CreateThread(function()
-        local currentVersion = GetResourceMetadata('dolu_tool', 'version', 0)
+	local currentVersion = GetResourceMetadata('dolu_tool', 'version', 0)
 
-        if currentVersion then
-            currentVersion = currentVersion:match('%d%.%d+%.%d+')
-        end
+	if currentVersion then
+		currentVersion = currentVersion:match('%d%.%d+%.%d+')
+	end
 
-        if not currentVersion then return print("^1Unable to determine current resource version for 'dolu_tool' ^0") end
+	if not currentVersion then return print("^1Unable to determine current resource version for 'dolu_tool' ^0") end
 
-        local data
-        PerformHttpRequest('https://api.github.com/repos/dolutattoo/dolu_tool/releases/latest', function(status, response)
-            if status ~= 200 then return end
+	SetTimeout(1000, function()
+		PerformHttpRequest('https://api.github.com/repos/dolutattoo/dolu_tool/releases/latest', function(status, response)
+			if status ~= 200 then return end
 
-            response = json.decode(response)
-            if response.prerelease then return end
+			response = json.decode(response)
+			if response.prerelease then return end
 
-            local latestVersion = response.tag_name:find('%d%.%d+%.%d+')
-            if not latestVersion or latestVersion == currentVersion then return end
+			local latestVersion = response.tag_name:match('%d%.%d+%.%d+')
+			if not latestVersion or latestVersion == currentVersion then return end
 
             local cv = { string.strsplit('.', currentVersion) }
             local lv = { string.strsplit('.', latestVersion) }
@@ -28,20 +27,16 @@ local function checkVersion()
 
                 if current ~= minimum then
                     if current < minimum then
-                        data = { currentVersion = currentVersion, url = response.html_url }
+                        versionData = { currentVersion = currentVersion, url = response.html_url }
                         print(("^3An update is available for 'dolu_tool' (current version: %s)\r\n%s^0"):format(currentVersion, response.html_url))
                     else
-                        data = { currentVersion = currentVersion }
+                        versionData = { currentVersion = currentVersion, url = response.html_url }
                     end
                     break
                 end
             end
-        end, 'GET')
-        
-        local timeout = 0
-        while not data or timeout == 2000 do Wait(0); timeout += 1 end
-        versionData = data or { currentVersion = currentVersion }
-    end)
+		end, 'GET')
+	end)
 end
 
 if not Config.development then
