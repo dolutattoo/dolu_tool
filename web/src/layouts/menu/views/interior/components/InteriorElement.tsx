1acb3151d0b07038d947c92bca7361fb6020428f
Dolu <alfrider02@gmail.com>
2023-01-20 03:53:34 +0100

    fix(web/interior): room count locale

--- web/src/layouts/menu/views/interior/components/InteriorElement.tsx
+++ web/src/layouts/menu/views/interior/components/InteriorElement.tsx
@@ -32,7 +32,7 @@ const InteriorElement: React.FC = () => {
         <Text size={24} weight={600}>Current interior</Text>
         <Space h='xs' />
         <Group><Text>{locale.ui_interior_id}:</Text><Text color='blue.4' > { interior.interiorId }</Text></Group>
-        <Group><Text>{locale.ui_current_room}:</Text><Text color='blue.4' > { interior.roomCount }</Text></Group>
+        <Group><Text>{locale.ui_room_count}:</Text><Text color='blue.4' > { interior.roomCount }</Text></Group>
         <Group><Text>{locale.ui_portal_count}:</Text><Text color='blue.4' > { interior.portalCount }</Text></Group>
         <Group><Text>{locale.ui_current_room}:</Text><Text color='blue.4' > { interior.currentRoom?.index } - { interior.currentRoom?.name }</Text></Group>
       </Paper>
