3cf654cbb47e55ea35a48e9ab31aa9fc9cda8355
Dolu <alf.rider@hotmail.fr>
2023-05-31 01:30:36 +0200

    miscs(client): relocate function & format strings

--- client/interior.lua
+++ client/interior.lua
@@ -103,67 +103,6 @@ RegisterNUICallback('dolu_tool:setPortalFlagCheckbox', function(data, cb)
     cb(1)
 end)
 
-function DrawPortalInfos(interiorId)
-    local ix, iy, iz = GetInteriorPosition(interiorId)
-    local rotX, rotY, rotZ, rotW = GetInteriorRotation(interiorId)
-    local interiorPosition = vec3(ix, iy, iz)
-    local interiorRotation = quat(rotW, rotX, rotY, rotZ)
-    local pedCoords = GetEntityCoords(cache.ped)
-
-    for portalId = 0, GetInteriorPortalCount(interiorId) - 1 do
-        local corners = {}
-        local pureCorners = {}
-
-        for cornerIndex = 0, 3 do
-            local cornerX, cornerY, cornerZ = GetInteriorPortalCornerPosition(interiorId, portalId, cornerIndex)
-            local cornerPosition = interiorPosition + Utils.QMultiply(interiorRotation, vec3(cornerX, cornerY, cornerZ))
-
-            corners[cornerIndex] = cornerPosition
-            pureCorners[cornerIndex] = vec3(cornerX, cornerY, cornerZ)
-        end
-
-        local CrossVector = Utils.Lerp(corners[0], corners[2], 0.5)
-
-        if #(pedCoords - CrossVector) <= 8.0 then
-            if Client.portalPoly then
-                DrawPoly(corners[0].x, corners[0].y, corners[0].z, corners[1].x, corners[1].y, corners[1].z, corners[2].x, corners[2].y, corners[2].z, 0, 0, 180, 150)
-                DrawPoly(corners[0].x, corners[0].y, corners[0].z, corners[2].x, corners[2].y, corners[2].z, corners[3].x, corners[3].y, corners[3].z, 0, 0, 180, 150)
-                DrawPoly(corners[3].x, corners[3].y, corners[3].z, corners[2].x, corners[2].y, corners[2].z, corners[1].x, corners[1].y, corners[1].z, 0, 0, 180, 150)
-                DrawPoly(corners[3].x, corners[3].y, corners[3].z, corners[1].x, corners[1].y, corners[1].z, corners[0].x, corners[0].y, corners[0].z, 0, 0, 180, 150)
-            end
-
-            if Client.portalLines then
-                -- Borders oultine
-                DrawLine(corners[0].x, corners[0].y, corners[0].z, corners[1].x, corners[1].y, corners[1].z, 0, 255, 0, 255)
-                DrawLine(corners[1].x, corners[1].y, corners[1].z, corners[2].x, corners[2].y, corners[2].z, 0, 255, 0, 255)
-                DrawLine(corners[2].x, corners[2].y, corners[2].z, corners[3].x, corners[3].y, corners[3].z, 0, 255, 0, 255)
-                DrawLine(corners[3].x, corners[3].y, corners[3].z, corners[0].x, corners[0].y, corners[0].z, 0, 255, 0, 255)
-
-                -- Middle lines
-                DrawLine(corners[0].x, corners[0].y, corners[0].z, corners[2].x, corners[2].y, corners[2].z, 0, 255, 0, 255)
-                DrawLine(corners[1].x, corners[1].y, corners[1].z, corners[3].x, corners[3].y, corners[3].z, 0, 255, 0, 255)
-            end
-
-            if Client.portalCorners then
-                Utils.Draw3DText(corners[0], '~b~C0:~w~ ' .. math.round(pureCorners[0].x, 2) .. ' ' .. math.round(pureCorners[0].y, 2) .. ' ' .. math.round(pureCorners[0].z, 2))
-                Utils.Draw3DText(corners[1], '~b~C1:~w~ ' .. math.round(pureCorners[1].x, 2) .. ' ' .. math.round(pureCorners[1].y, 2) .. ' ' .. math.round(pureCorners[1].z, 2))
-                Utils.Draw3DText(corners[2], '~b~C2:~w~ ' .. math.round(pureCorners[2].x, 2) .. ' ' .. math.round(pureCorners[2].y, 2) .. ' ' .. math.round(pureCorners[2].z, 2))
-                Utils.Draw3DText(corners[3], '~b~C3:~w~ ' .. math.round(pureCorners[3].x, 2) .. ' ' .. math.round(pureCorners[3].y, 2) .. ' ' .. math.round(pureCorners[3].z, 2))
-            end
-
-            if Client.portalInfos then
-                local portalFlags = GetInteriorPortalFlag(interiorId, portalId)
-                local portalRoomTo = GetInteriorPortalRoomTo(interiorId, portalId)
-                local portalRoomFrom = GetInteriorPortalRoomFrom(interiorId, portalId)
-
-                Utils.Draw3DText(vec3(CrossVector.x, CrossVector.y, CrossVector.z + 0.2), '~b~Portal ~w~' .. portalId)
-                Utils.Draw3DText(vec3(CrossVector.x, CrossVector.y, CrossVector.z + 0.05), '~b~From ~w~' .. portalRoomFrom .. '~b~ To ~w~' .. portalRoomTo)
-                Utils.Draw3DText(vec3(CrossVector.x, CrossVector.y, CrossVector.z - 0.1), '~b~Flags ~w~' .. portalFlags)
-            end
-        end
-    end
-end
-
 RegisterNUICallback('dolu_tool:setTimecycle', function(data, cb)
     cb(1)
 
--- client/threads.lua
+++ client/threads.lua
@@ -25,7 +25,64 @@ CreateThread(function()
     while true do
         if Client.interiorId > 0 then
             if Client.portalPoly or Client.portalLines or Client.portalCorners or Client.portalInfos then
-                DrawPortalInfos(Client.interiorId)
+                local ix, iy, iz = GetInteriorPosition(Client.interiorId)
+                local rotX, rotY, rotZ, rotW = GetInteriorRotation(Client.interiorId)
+                local interiorPosition = vec3(ix, iy, iz)
+                local interiorRotation = quat(rotW, rotX, rotY, rotZ)
+                local pedCoords = GetEntityCoords(cache.ped)
+
+                for portalId = 0, GetInteriorPortalCount(Client.interiorId) - 1 do
+                    local corners = {}
+                    local pureCorners = {}
+
+                    for cornerIndex = 0, 3 do
+                        local cornerX, cornerY, cornerZ = GetInteriorPortalCornerPosition(Client.interiorId, portalId, cornerIndex)
+                        local cornerPosition = interiorPosition + Utils.QMultiply(interiorRotation, vec3(cornerX, cornerY, cornerZ))
+
+                        corners[cornerIndex] = cornerPosition
+                        pureCorners[cornerIndex] = vec3(cornerX, cornerY, cornerZ)
+                    end
+
+                    local CrossVector = Utils.Lerp(corners[0], corners[2], 0.5)
+
+                    if #(pedCoords - CrossVector) <= 8.0 then
+                        if Client.portalPoly then
+                            DrawPoly(corners[0].x, corners[0].y, corners[0].z, corners[1].x, corners[1].y, corners[1].z, corners[2].x, corners[2].y, corners[2].z, 0, 0, 180, 150)
+                            DrawPoly(corners[0].x, corners[0].y, corners[0].z, corners[2].x, corners[2].y, corners[2].z, corners[3].x, corners[3].y, corners[3].z, 0, 0, 180, 150)
+                            DrawPoly(corners[3].x, corners[3].y, corners[3].z, corners[2].x, corners[2].y, corners[2].z, corners[1].x, corners[1].y, corners[1].z, 0, 0, 180, 150)
+                            DrawPoly(corners[3].x, corners[3].y, corners[3].z, corners[1].x, corners[1].y, corners[1].z, corners[0].x, corners[0].y, corners[0].z, 0, 0, 180, 150)
+                        end
+
+                        if Client.portalLines then
+                            -- Borders oultine
+                            DrawLine(corners[0].x, corners[0].y, corners[0].z, corners[1].x, corners[1].y, corners[1].z, 0, 255, 0, 255)
+                            DrawLine(corners[1].x, corners[1].y, corners[1].z, corners[2].x, corners[2].y, corners[2].z, 0, 255, 0, 255)
+                            DrawLine(corners[2].x, corners[2].y, corners[2].z, corners[3].x, corners[3].y, corners[3].z, 0, 255, 0, 255)
+                            DrawLine(corners[3].x, corners[3].y, corners[3].z, corners[0].x, corners[0].y, corners[0].z, 0, 255, 0, 255)
+
+                            -- Middle lines
+                            DrawLine(corners[0].x, corners[0].y, corners[0].z, corners[2].x, corners[2].y, corners[2].z, 0, 255, 0, 255)
+                            DrawLine(corners[1].x, corners[1].y, corners[1].z, corners[3].x, corners[3].y, corners[3].z, 0, 255, 0, 255)
+                        end
+
+                        if Client.portalCorners then
+                            Utils.Draw3DText(corners[0], ('~b~C0:~w~ %s %s %s'):format(math.round(pureCorners[0].x, 2), math.round(pureCorners[0].y, 2), math.round(pureCorners[0].z, 2)))
+                            Utils.Draw3DText(corners[1], ('~b~C1:~w~ %s %s %s'):format(math.round(pureCorners[1].x, 2), math.round(pureCorners[1].y, 2), math.round(pureCorners[1].z, 2)))
+                            Utils.Draw3DText(corners[2], ('~b~C2:~w~ %s %s %s'):format(math.round(pureCorners[2].x, 2), math.round(pureCorners[2].y, 2), math.round(pureCorners[2].z, 2)))
+                            Utils.Draw3DText(corners[3], ('~b~C3:~w~ %s %s %s'):format(math.round(pureCorners[3].x, 2), math.round(pureCorners[3].y, 2), math.round(pureCorners[3].z, 2)))
+                        end
+
+                        if Client.portalInfos then
+                            local portalFlags = GetInteriorPortalFlag(Client.interiorId, portalId)
+                            local portalRoomTo = GetInteriorPortalRoomTo(Client.interiorId, portalId)
+                            local portalRoomFrom = GetInteriorPortalRoomFrom(Client.interiorId, portalId)
+
+                            Utils.Draw3DText(vec3(CrossVector.x, CrossVector.y, CrossVector.z + 0.2), ('~b~Portal ~w~%s'):format(portalId))
+                            Utils.Draw3DText(vec3(CrossVector.x, CrossVector.y, CrossVector.z + 0.05), ('~b~From ~w~%s~b~ To ~w~%s'):format(portalRoomFrom, portalRoomTo))
+                            Utils.Draw3DText(vec3(CrossVector.x, CrossVector.y, CrossVector.z - 0.1), ('~b~Flags ~w~%s'):format(portalFlags))
+                        end
+                    end
+                end
             end
         else
             Wait(500)
@@ -48,7 +105,7 @@ CreateThread(function()
                 SendNUIMessage({
                     action = 'playerCoords',
                     data = {
-                        coords = Utils.round(coords.x, 3) .. ', ' .. Utils.round(coords.y, 3) .. ', ' .. Utils.round(coords.z, 3),
+                        coords = ('%.3f, %.3f, %.3f'):format(Utils.round(coords.x, 3), Utils.round(coords.y, 3), Utils.round(coords.z, 3)),
                         heading = tostring(Utils.round(GetEntityHeading(cache.ped), 3))
                     }
                 })
