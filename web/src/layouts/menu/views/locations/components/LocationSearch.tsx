fd9e648ed75c4212f6cae823611af4fe9101fbd6
dolutattoo <alfrider02@gmail.com>
2024-04-30 04:37:57 +0200

    fix(web): remember last location search input

--- web/src/layouts/menu/views/locations/components/LocationSearch.tsx
+++ web/src/layouts/menu/views/locations/components/LocationSearch.tsx
@@ -1,23 +1,22 @@
 import { TextInput } from '@mantine/core'
 import { useDebouncedValue } from '@mantine/hooks'
 import { TbSearch } from 'react-icons/tb'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { locationCustomFilterAtom, locationsActivePageAtom, locationSearchAtom, locationVanillaFilterAtom } from '../../../../../atoms/location'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useLocales } from '../../../../../providers/LocaleProvider'
 import { fetchNui } from '../../../../../utils/fetchNui'
 
 const LocationSearch: React.FC = () => {
   const { locale } = useLocales()
-  const [search, setSearch] = useState('')
-  const setLocationsSearch = useSetRecoilState(locationSearchAtom)
+  const [search, setSearch] = useRecoilState(locationSearchAtom)
   const [debouncedSearch] = useDebouncedValue(search, 100)
   const setActivePage = useSetRecoilState(locationsActivePageAtom)
   const checkedVanilla = useRecoilValue(locationVanillaFilterAtom)
   const checkedCustom = useRecoilValue(locationCustomFilterAtom)
 
   useEffect(() => {
-    setLocationsSearch(debouncedSearch)
+    setActivePage(1)
     fetchNui('dolu_tool:loadPages', { type: 'locations', activePage: 1, filter: debouncedSearch, checkboxes: { vanilla: checkedVanilla, custom: checkedCustom } })
   }, [debouncedSearch])
 
@@ -27,10 +26,7 @@ const LocationSearch: React.FC = () => {
         placeholder={locale.ui_search}
         icon={<TbSearch size={20} />}
         value={search}
-        onChange={(e) => {
-          setActivePage(1)
-          setSearch(e.target.value)
-        }}
+        onChange={(e) => setSearch(e.target.value)}
       />
     </>
   )
