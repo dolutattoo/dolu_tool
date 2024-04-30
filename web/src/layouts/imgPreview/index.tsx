2faf0afed92d1f28e77842568c905bad526101c5
dolutattoo <alfrider02@gmail.com>
2024-04-30 04:11:12 +0200

    fix(web): correct offset for images previews

--- web/src/layouts/imgPreview/index.tsx
+++ web/src/layouts/imgPreview/index.tsx
@@ -10,19 +10,19 @@ const ImgPreview: React.FC = () => {
     <Transition transition='slide-right' mounted={isDisplayImage}>
       {(style) => (
         <Paper
-          radius='md'
-          p='md'
+          p='xs'
           shadow='xs'
           style={style}
           sx={{
             zIndex: 2,
             position: 'absolute',
             top: '2%',
-            left: '33.5%',
+            left: '650px',
+            maxWidth: '600px',
+            borderRadius: '10px',
           }}
         >
           <Image
-            height={300}
             fit='contain'
             alt={'Display selected image'}
             src={imagePath}
